7599e6b98c3294b56618435d2c4e12f913e4c8c3
Hassan Khalid <m.hassankhalid39@gmail.com>
2024-09-11 17:27:11 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# carsel_slider_plugin
+ ui/ux plugin 2 practice
---
+++ android/app/src/main/kotlin/com/example/carsel_slider_plugin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.carsel_slider_plugin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
